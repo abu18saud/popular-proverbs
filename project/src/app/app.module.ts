5e80610f4ea378e89c2b61b172669c7c71881606
Abdullah Alsalem <441226478@tvtc.edu.sa>
2022-06-10 16:49:22 +0300

    ...

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,21 +1,15 @@
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Toolbar1Component } from './theme/components/toolbar1/toolbar1.component';
-import { LangComponent } from './theme/components/lang/lang.component';
-import { MaterialModule } from './metrial.module';
 
 // import { Toolbar1Component } from './theme/components/toolbar1/toolbar1.component';
 // import { Toolbar2Component } from './theme/components/toolbar2/toolbar2.component';
 // import { UserMenuComponent } from './theme/components/user-menu/user-menu.component';
 // import { CurrencyComponent } from './theme/components/currency/currency.component';
+// import { LangComponent } from './theme/components/lang/lang.component';
 // import { SocialIconsComponent } from './theme/components/social-icons/social-icons.component';
 // import { ContactsComponent } from './theme/components/contacts/contacts.component'; 
 // import { HorizontalMenuComponent } from './theme/components/menu/horizontal-menu/horizontal-menu.component';
@@ -23,34 +17,14 @@ import { MaterialModule } from './metrial.module';
 // import { FooterComponent } from './theme/components/footer/footer.component';
 
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-
-
 @NgModule({
   declarations: [
     AppComponent,
-    Toolbar1Component,
-    LangComponent    
+    Toolbar1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      defaultLanguage: 'ar',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })        
-  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
-  providers: [
+    AppRoutingModule
   ],
 })
 export class AppModule { }
